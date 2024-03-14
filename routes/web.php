94c81cab46914bea036dc9b09187e9970b6ddf44
eddielin1223 <eddielin1223@gmail.com>
2024-03-14 17:58:20 +0800

    first commit for glossika request 1, 2, 3

---
+++ routes/web.php
@@ -0,0 +1,53 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use Illuminate\Http\Request;
+use Illuminate\Foundation\Auth\EmailVerificationRequest;
+use Illuminate\Support\Facades\Log;
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::middleware([
+    'auth:sanctum',
+    config('jetstream.auth_session'),
+    'verified'
+])->group(function () {
+    Route::get('/dashboard', function () {
+        Log::info('dash');
+        return view('dashboard');
+    })->name('dashboard');
+
+    Route::get('/recommendation', function () {
+        Log::info('recomm');
+        return view('dashboard');
+    })->name('recommendation');
+});
+
+Route::get('/email/verify', function () {
+    Log::info('email-verify');
+    return view('auth.verify-email');
+})->middleware('auth')->name('verification.notice');
+
+Route::get('/email/verify/{id}/{hash}', function (EmailVerificationRequest $request) {
+    Log::info('email-verify-id');
+    $request->fulfill();
+    return redirect('/dashboard');
+})->middleware(['auth', 'signed'])->name('verification.verify');
+
+Route::post('/email/verification-notification', function (Request $request) {
+    $request->user()->sendEmailVerificationNotification();
+ 
+    return back()->with('message', 'Verification link sent!');
+})->middleware(['auth', 'throttle:6,1'])->name('verification.send');
